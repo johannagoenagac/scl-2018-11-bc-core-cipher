2d5659346bb6a72864173261ae6f82336f5b2fac
Johanna Goenaga <johannagoenagac@gmail.com>
2018-12-04 13:54:52 -0300

    Actualizaciones en Html, Css y Js

--- cipher.js
+++ cipher.js
@@ -7,21 +7,39 @@ let result="";
 
     for (let i=0; i < textElement.length; i++){
       let ascii =textElement.charCodeAt(i);
+      if (ascii >= 65 && ascii <= 90){
       let asciiOffset =(ascii - 65 + offsetNumber)%26 + 65;
-
       result +=String.fromCharCode(asciiOffset);
-
-
+    }else if (ascii >= 97 && ascii<= 122){
+      let asciiOffset =(ascii - 97 + offsetNumber) %26 + 97;
+				result += String.fromCharCode(asciiOffset);
     }
-// console.log(result);
+
+  }
+
     return result;
 
   },
 
 
-  decode: () => {
-    /* Acá va tu código */
-  }
+  decode: (textElement,offsetNumber) => {
 
+    let result="";
+
+        for (let i=0; i < textElement.length; i++){
+          let ascii =textElement.charCodeAt(i);
+          if (ascii >= 65 && ascii <= 90){
+          let asciiOffset =(ascii - 65 - offsetNumber)%26 + 65;
+          result +=String.fromCharCode(asciiOffset);
+        }else if (ascii >= 97 && ascii<= 122){
+          let asciiOffset =(ascii - 97 +offsetNumber) %26 + 97;
+    				result += String.fromCharCode(asciiOffset);
+        }
+
+}
+
+
+    return result;
+  },
 
 }
