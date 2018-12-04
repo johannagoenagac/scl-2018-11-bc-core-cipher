c5111e2f970e8e58b120c00b55d183f2f1007803
Johanna Goenaga <johannagoenagac@gmail.com>
2018-12-04 13:54:52 -0300

    Actualizaciones en Html, Css y Js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 window.onload =() =>{
+
+
   document.getElementById("btnEncode").addEventListener("click",
   (evento)=> {
     evento.preventDefault();
@@ -15,11 +17,18 @@ document.getElementById("btnDecode").addEventListener("click",
   evento.preventDefault();
 
   let textElement = document.getElementById("text").value.toUpperCase();
-
   let offsetNumber = parseInt(document.getElementById("offset").value);
-
   let result = cipher.decode(textElement,offsetNumber);
   document.getElementById("resultado").innerHTML = result;
 
 })
+
+document.getElementById("btnEntrar").addEventListener("click",
+(evento) => {
+  evento.preventDefault();
+
+  document.getElementById("screen").style.display="none";
+  document.getElementById("screen2").style.display="block";
+})
+
 }
